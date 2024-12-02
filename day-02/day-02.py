74156a70e6b6c4d466991372278dab9761aeb666
Espiobest <59823894+Espiobest@users.noreply.github.com>
2024-12-02 00:20:04 -0500

    add day2 py sol

--- day-02/day-02.py
+++ day-02/day-02.py
@@ -1,2 +1,20 @@
+
+def safe(lst):
+    monotone = lst == sorted(lst) or lst == sorted(lst, reverse=True)
+    if not monotone:
+        return False
+    for i, j in zip(lst, lst[1:]):
+        if abs(i-j) < 1 or abs(i-j) > 3:
+            return False
+    return True
+
 with open('data.txt', 'r') as r:
     lines = r.read().splitlines()
+    s = 0
+    s2 = 0
+    for i in lines:
+        nums = [*map(int, i.split())]
+        s += safe(nums)
+        s2 += any(safe(nums[:j] + nums[j+1:]) for j in range(len(nums)))
+    print("Part 1:", s)
+    print("Part 2:", s2)
