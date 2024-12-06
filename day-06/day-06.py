40de48bc0dd357b643f3712ae8a106a0e242f13f
Espiobest <59823894+Espiobest@users.noreply.github.com>
2024-12-06 02:54:14 -0500

    add day6 py sol (bruteforce :))

--- day-06/day-06.py
+++ day-06/day-06.py
@@ -1,2 +1,55 @@
 with open('data.txt', 'r') as r:
-    lines = r.read().splitlines()
+    grid = r.read().splitlines()
+    grid = [list(row) for row in grid]
+
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        if grid[i][j] == "^":
+            start = (i, j)
+            break
+
+DIR = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # N, E, S, W
+
+def path(grid, part2=False):
+    loop = True
+    pos = set()
+    visited = set()
+    nx, ny = start
+    cur_dir = 0
+    while True:
+        if nx < 0 or ny < 0 or nx >= len(grid) or ny >= len(grid[0]):
+            loop = False
+            break
+
+        if grid[nx][ny] != "#":
+            if part2 and (nx, ny, cur_dir) in visited:
+                return None 
+            visited.add((nx, ny, cur_dir))
+            pos.add((nx, ny))
+        else:
+            nx -= DIR[cur_dir][0]
+            ny -= DIR[cur_dir][1]
+            cur_dir = (cur_dir + 1) % 4
+
+        nx, ny = (nx + DIR[cur_dir][0], ny + DIR[cur_dir][1])
+
+    return loop, pos
+
+
+_, pos = path(grid)
+part1 = len(pos)
+print("Part 1:", part1)
+
+changePos = set()
+# check all possible obstructions
+for x, y in pos:
+    if (x, y) == start:
+        continue
+
+    grid2 = [row[:] for row in grid]
+    grid2[x][y] = "#"
+    if path(grid2, part2=True) is None:
+        changePos.add((x, y))
+
+part2 = len(changePos)
+print("Part 2:", part2)
