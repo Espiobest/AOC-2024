a268ae0d2495fa112aab90ddd5cad690229f76a3
Espiobest <59823894+Espiobest@users.noreply.github.com>
2024-12-10 02:46:01 -0500

    day9, day10 sols

--- day-09/day-09.py
+++ day-09/day-09.py
@@ -1,2 +1,72 @@
 with open('data.txt', 'r') as r:
-    lines = r.read().splitlines()
+    nums = r.read()
+
+data = []
+dots = []
+curID = 0
+for i in range(len(nums)):
+    num = int(nums[i])
+    if i % 2 == 0:
+        data.extend([curID] * num)
+        curID += 1
+    else:
+        data.extend(['.'] * num)
+        dots.extend(range(len(data)-num, len(data)))
+
+data2 = data.copy()
+
+for idx in dots:
+    if idx > len(data):
+        break
+    val = data.pop()
+    data[idx] = val
+    while data[-1] == '.':
+        data.pop()
+
+part1 = 0
+for i, e in enumerate(data):
+    part1 += int(e) * i
+
+print("Part 1:", part1)
+
+# Part 2
+
+dot_groups = []
+for i in range(len(data2)):
+    if data2[i] == '.':
+        if i == 0 or data2[i - 1] != '.':
+            dot_groups.append([i])
+        else:
+            dot_groups[-1].append(i)
+
+file_data = []
+i = 0
+while i < len(data2):
+    if data2[i] != '.':
+        file_id = data2[i]
+        start = i
+        while i < len(data2) and data2[i] == file_id:
+            i += 1
+        size = i - start
+        file_data.append((file_id, size, start))
+    else:
+        i += 1
+
+file_data = file_data[::-1]
+for file_id, size, start_index in file_data:
+    for j, dots in enumerate(dot_groups):
+        if len(dots) >= size and dots[0] < start_index:  
+            for k in range(size):
+                data2[dots[k]] = file_id
+                data2[start_index + k] = '.'
+            dot_groups[j] = dots[size:]
+            if len(dot_groups[j]) == 0:
+                dot_groups.pop(j)
+            break
+
+part2 = 0
+for i, e in enumerate(data2):
+    if e != '.':
+        part2 += int(e) * i
+
+print("Part 2:", part2)
--- day-10/day-10.py
+++ day-10/day-10.py
@@ -1,2 +1,42 @@
 with open('data.txt', 'r') as r:
     lines = r.read().splitlines()
+
+grid = [[*map(int, line)] for line in lines]
+
+def get_neighbors(x, y):
+    return [(x, y-1), (x, y+1), (x-1, y), (x+1, y)]
+
+start_vals = []
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        if grid[i][j] == 0:
+            start_vals.append((i, j))
+
+def get_paths(start, part2=False):
+    score = 0
+    stack = [start]
+    visited = set()
+    while stack:
+        x, y = stack.pop()
+        height = grid[x][y]
+        if height == 9:
+            if (x, y) not in visited or part2:
+                score += 1
+                visited.add((x, y))
+
+        for nx, ny in get_neighbors(x, y):
+            if nx < 0 or nx >= len(grid) or ny < 0 or ny >= len(grid[0]):
+                continue
+            if grid[nx][ny] == height + 1:
+                stack.append((nx, ny))
+
+    return score
+
+part1 = 0
+part2 = 0
+for start in start_vals:
+    part1 += get_paths(start)
+    part2 += get_paths(start, part2=True)
+
+print("Part 1:", part1)
+print("Part 2:", part2)
